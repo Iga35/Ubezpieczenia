82e6967e4b0e993848732dd7ec20f80da5b6b60f
Iga Dźwigała <s35268@pjwstk.edu.pl>
2026-04-01 11:12:52 +0200

    Main, zad 1 polisa

---
+++ Ubezpieczenia/src/Polisa.java
@@ -0,0 +1,29 @@
+public class Polisa {
+    private String numerPolisy;
+    private String klient;
+    private double skladkaBazowa;
+    private int poziomRyzyka;
+    private double wartoscPojazdu;
+    private boolean czyMaAlarm;
+    private boolean czyBezszkodowyKlient;
+    private static int liczbaUtworzonychPolis;
+    private static final double OPLATA_ADMINISTRACYJNA=100;
+
+    public Polisa(String numerPolisy, String klient, double skladkaBazowa, int poziomRyzyka,
+                  double wartoscPojazdu, boolean czyMaAlarm, boolean czyBezszkodowyKlient){
+        this.numerPolisy=numerPolisy;
+        this.klient=klient;
+        this.skladkaBazowa=skladkaBazowa;
+        this.poziomRyzyka=poziomRyzyka;
+        this.wartoscPojazdu=wartoscPojazdu;
+        this.czyMaAlarm=czyMaAlarm;
+        this.czyBezszkodowyKlient=czyBezszkodowyKlient;
+        liczbaUtworzonychPolis++;
+    }
+    public double obliczSkladkeKoncowa(){
+        
+    }
+
+
+
+}
